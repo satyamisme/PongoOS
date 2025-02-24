da43bc8fe6e6ff3b25e71ab28ad7ce3ba45a0720
Siguza <siguza@siguza.net>
2025-02-24 17:00:52 +0100

    Restructure usbloader

--- src/shell/usbloader.c
+++ src/shell/usbloader.c
@@ -29,45 +29,36 @@
 #include <mach-o/nlist.h>
 #include <mach-o/reloc.h>
 
+extern void queue_rx_char(char inch);
+
 #define UPLOADSZ (1024 * 1024)
 #define UPLOADSZ_MAX (1024 * 1024 * 128)
 
-uint8_t * loader_xfer_recv_data;
-uint32_t loader_xfer_recv_count;
+static bool usbloader_is_waiting_xfer = false;
+
+uint8_t *loader_xfer_recv_data;
 uint32_t loader_xfer_recv_size;
-uint32_t loader_next_xfer_size;
-uint32_t loader_xfer_size;
-extern uint64_t vatophys(uint64_t kvaddr);
-char usbloader_is_waiting_xfer;
-void usbloader_xfer_done_cb(void* data, uint32_t size, uint32_t transferred) {
-    if (!usbloader_is_waiting_xfer) return;
-    cache_invalidate(loader_xfer_recv_data, loader_xfer_recv_count);
-    loader_xfer_recv_count = transferred;
-    usbloader_is_waiting_xfer = 0;
+uint32_t loader_xfer_recv_count;
+static uint32_t loader_xfer_size;
+
+void usbloader_init(void)
+{
+    loader_xfer_recv_data = alloc_contig(UPLOADSZ);
+    loader_xfer_size = UPLOADSZ;
+    loader_xfer_recv_size = UPLOADSZ;
+    loader_xfer_recv_count = 0;
 }
 
-char cmd_buf[256];
-uint32_t cmd_len;
-char should_wait_for_cmd_handler = 0;
-extern void queue_rx_char(char inch);
-bool usb_write_stdin(const void *data, uint32_t size) {
-    enable_interrupts();
-    const char* datac = (const char*) data;
-    for (int i=0; i<size; i++) {
-        if (!datac[i]) break;
-        queue_rx_char(datac[i]);
+void resize_loader_xfer_data(uint32_t newsz)
+{
+    if(newsz > UPLOADSZ_MAX)
+    {
+        panic("resize_loader_xfer_data");
     }
-    if (should_wait_for_cmd_handler)
-        event_wait(&command_handler_iter);
     disable_interrupts();
-    return true;
-}
-
-void resize_loader_xfer_data(uint32_t newsz) {
-    if (newsz > UPLOADSZ_MAX) panic("resize_loader_xfer_data");
-    disable_interrupts();
-    if (newsz > loader_xfer_recv_size) {
-        uint8_t* new_xfer_buffer = alloc_contig(newsz);
+    if(newsz > loader_xfer_recv_size)
+    {
+        uint8_t *new_xfer_buffer = alloc_contig(newsz);
         memcpy(new_xfer_buffer, loader_xfer_recv_data, loader_xfer_recv_count);
         free_contig(loader_xfer_recv_data, loader_xfer_recv_size);
         loader_xfer_recv_size = newsz;
@@ -75,95 +66,172 @@ void resize_loader_xfer_data(uint32_t newsz) {
     }
     enable_interrupts();
 }
-bool reallocate_loader_xfer_data(const void* data, uint32_t size) {
-    if (size != 4) panic("reallocate_loader_xfer_data");
-    
-    uint32_t newsz = *(uint32_t*)data;
-    newsz += 0x1ff;
-    newsz &= ~0x1ff;
-    if (newsz > UPLOADSZ_MAX) return false;
-    loader_xfer_recv_count = 0;
-    usbloader_is_waiting_xfer = 1;
-    resize_loader_xfer_data(newsz);
-    loader_xfer_size = newsz;
-    usb_out_transfer_dma(2, loader_xfer_recv_data, vatophys((uint64_t)loader_xfer_recv_data), loader_xfer_size, usbloader_xfer_done_cb); // should resolve the VA rather than doing this, but oh well.
 
+static void usbloader_bulk_upload_done(void *data, uint32_t size, uint32_t transferred)
+{
+    if(!usbloader_is_waiting_xfer)
+    {
+        panic("usbloader_bulk_upload_done");
+    }
+    // XXX: this seems both wrong and unnecessary since ep_out_recv_data_done() should invalidate already?
+    //cache_invalidate(loader_xfer_recv_data, loader_xfer_recv_count);
+    loader_xfer_recv_count = transferred;
+    usbloader_is_waiting_xfer = false;
+}
+
+static bool usbloader_bulk_upload_start(const void *data, uint32_t size)
+{
+    uint32_t newsz = 0;
+    if(data)
+    {
+        if(size != 4)
+        {
+            panic("usbloader_bulk_upload_start");
+        }
+        // Round up to USB BULK packet size
+        newsz = (*(uint32_t*)data + 0x1ff) & ~0x1ff;
+        if(newsz > UPLOADSZ_MAX)
+        {
+            return false;
+        }
+    }
+    loader_xfer_recv_count = 0;
+    usbloader_is_waiting_xfer = true;
+    if(data)
+    {
+        resize_loader_xfer_data(newsz);
+        loader_xfer_size = newsz;
+    }
+    usb_out_transfer_dma(2, loader_xfer_recv_data, vatophys((uint64_t)loader_xfer_recv_data), loader_xfer_size, usbloader_bulk_upload_done); // should resolve the VA rather than doing this, but oh well.
     return true;
 }
 
-void usbloader_init(void) {
-    loader_xfer_recv_data = alloc_contig(UPLOADSZ);
-    loader_xfer_size = UPLOADSZ;
-    loader_xfer_recv_size = UPLOADSZ;
-    loader_xfer_recv_count = 0;
-} // fetch_stdoutbuf
+static bool should_wait_for_cmd_handler = false;
 
-char stdoutbuf_copy[STDOUT_BUFLEN];
-void usb_read_stdout_cb(void) {
-
+static bool usbloader_write_stdin(const void *data, uint32_t size)
+{
+    enable_interrupts();
+    const char *cmd = (const char*)data;
+    for(uint32_t i = 0; i < size && cmd[i] != '\0'; ++i)
+    {
+        queue_rx_char(cmd[i]);
+    }
+    if(should_wait_for_cmd_handler)
+    {
+        event_wait(&command_handler_iter);
+    }
+    disable_interrupts();
+    return true;
 }
-bool ep0_device_request(struct setup_packet *setup) {
-    if (setup->bmRequestType == 0x21) {
-        if (setup->bRequest == 1 && setup->wLength == 0) { // request bulk upload initialization
-            if (usbloader_is_waiting_xfer) return false;
-            loader_xfer_recv_count = 0;
-            usbloader_is_waiting_xfer = 1;
-            usb_out_transfer_dma(2, loader_xfer_recv_data, vatophys((uint64_t)loader_xfer_recv_data), loader_xfer_size, usbloader_xfer_done_cb); // should resolve the VA rather than doing this, but oh well.
-            return true;
-        }
-        if (setup->bRequest == 2 && setup->wLength == 0) { // discard loaded data
-            if (!usbloader_is_waiting_xfer)
-                loader_xfer_recv_count = 0;
-            return true;
-        }
-        if (setup->bRequest == 3 && setup->wLength > 0 && setup->wLength <= 512) { // write to stdin
-            ep0_begin_data_out_stage(usb_write_stdin);
-            return true;
-        }
-        if (setup->bRequest == 4) {
-            if(setup->wValue == 0) // make it so next write to stdin will stall until command is over
+
+bool ep0_device_request(struct setup_packet *setup)
+{
+    switch(setup->bmRequestType)
+    {
+        case 0x21: // OUT (HOST2DEVICE)
+            switch(setup->bRequest)
             {
-                should_wait_for_cmd_handler = 1;
-                set_stdout_blocking(false);
-                return true;
+                case 1: // initiate bulk upload
+                    // TODO: re-evaluate possibility of aborting at this level?
+                    if(setup->wValue != 0 || setup->wIndex != 0 || usbloader_is_waiting_xfer)
+                    {
+                        return false;
+                    }
+                    if(setup->wLength == 4) // specify new buffer size
+                    {
+                        ep0_begin_data_out_stage(usbloader_bulk_upload_start);
+                        return true;
+                    }
+                    if(setup->wLength == 0) // use existing buffer size
+                    {
+                        usbloader_bulk_upload_start(NULL, 0);
+                        return true;
+                    }
+                    break;
+
+                case 2: // discard uploaded data
+                    if(setup->wValue != 0 || setup->wIndex != 0 || setup->wLength != 0)
+                    {
+                        return false;
+                    }
+                    // TODO: support for abort?
+                    if(!usbloader_is_waiting_xfer)
+                    {
+                        loader_xfer_recv_count = 0;
+                    }
+                    return true;
+
+                case 3: // write to stdin
+                    if(setup->wValue != 0 || setup->wIndex != 0 || setup->wLength == 0 || setup->wLength > 512)
+                    {
+                        return false;
+                    }
+                    ep0_begin_data_out_stage(usbloader_write_stdin);
+                    return true;
+
+                case 4: // change stdio behaviour
+                    if(setup->wIndex != 0 || setup->wLength != 0)
+                    {
+                        return false;
+                    }
+                    switch(setup->wValue)
+                    {
+                        case 0: // make stdin block until the command returns
+                            should_wait_for_cmd_handler = true;
+                            set_stdout_blocking(false);
+                            return true;
+                            break;
+
+                        case 1: // make stdout block until async check-in
+                            should_wait_for_cmd_handler = false;
+                            set_stdout_blocking(true);
+                            return true;
+                            break;
+
+                        case 0xffff: // reset all
+                            should_wait_for_cmd_handler = false;
+                            set_stdout_blocking(false);
+                            return true;
+                            break;
+                    }
+                    break;
             }
-            if(setup->wValue == 1) // make writes to stdout stall until async check-in
+            break;
+
+        case 0xa1: // IN (DEVICE2HOST)
+            switch(setup->bRequest)
             {
-                should_wait_for_cmd_handler = 0;
-                set_stdout_blocking(true);
-                return true;
+                case 1: // read from stdout
+                {
+                    if(setup->wValue != 0 || setup->wIndex != 0 || (setup->wLength != 512 && setup->wLength != 0x1000))
+                    {
+                        return false;
+                    }
+                    static char stdoutbuf_copy[STDOUT_BUFLEN];
+                    int xferlen = setup->wLength;
+                    char *buf = stdoutbuf_copy;
+                    fetch_stdoutbuf(buf, &xferlen);
+                    if(xferlen > setup->wLength)
+                    {
+                        buf += xferlen - setup->wLength;
+                        xferlen = setup->wLength;
+                    }
+                    ep0_begin_data_in_stage(buf, xferlen, NULL);
+                    return true;
+                }
+
+                case 2: // check for async command completion status
+                {
+                    if(setup->wValue != 0 || setup->wIndex != 0 || setup->wLength != 1)
+                    {
+                        return false;
+                    }
+                    uint8_t inprog = command_in_progress;
+                    ep0_begin_data_in_stage(&inprog, 1, NULL);
+                    return true;
+                }
             }
-            if(setup->wValue == 0xffff) // reset all
-            {
-                should_wait_for_cmd_handler = 0;
-                set_stdout_blocking(false);
-                return true;
-            }
-        }
-        if (setup->bRequest == 1 && setup->wLength == 4) { // request upload buffer size change
-            if (usbloader_is_waiting_xfer) return false;
-            ep0_begin_data_out_stage(reallocate_loader_xfer_data);
-            return true;
-        }
-    } else if (setup->bmRequestType == 0xA1) {
-        // IN request
-        if (setup->bRequest == 1 && (setup->wLength == 512 || setup->wLength == 0x1000)) { // request bulk upload initialization
-            int xferlen = 0;
-            char *buf = stdoutbuf_copy;
-            fetch_stdoutbuf(buf, &xferlen);
-            if(xferlen > setup->wLength)
-            {
-                buf += xferlen - setup->wLength;
-                xferlen = setup->wLength;
-            }
-            ep0_begin_data_in_stage(buf, xferlen, usb_read_stdout_cb);
-            return true;
-        }
-        if (setup->bRequest == 2 && setup->wLength == 1) { // check for async command completion status
-            uint8_t inprog = command_in_progress;
-            ep0_begin_data_in_stage(&inprog, 1, usb_read_stdout_cb);
-            return true;
-        }
+            break;
     }
     return false;
 }
